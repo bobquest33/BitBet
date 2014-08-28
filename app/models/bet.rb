4444a1c497de3ce52b508f99e7d49655c21b75bc
Chad Mazzola <chad.mazzola@gmail.com>
2014-08-28 21:31:44 +0200

    More accurate validations
    - Email: judge
    - BTC: you, friend

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,7 +1,7 @@
 class Bet < ActiveRecord::Base
   validates :description, presence: true
-  validates :amount, presence: true
+  validates :amount, presence: true, format: /\A\d*\.{0,1}\d*\z/
   validates :you, presence: true
   validates :friend, presence: true
-  validates :judge, presence: true
+  validates :judge, presence: true, email: true
 end
