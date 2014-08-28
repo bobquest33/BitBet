424ff265fbda98213e6fdbe588c1362d82955543
Chad Mazzola <chad.mazzola@gmail.com>
2014-08-28 21:15:55 +0200

    Validate bet values
    All must be present

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,2 +1,7 @@
 class Bet < ActiveRecord::Base
+  validates :description, presence: true
+  validates :amount, presence: true
+  validates :you, presence: true
+  validates :friend, presence: true
+  validates :judge, presence: true
 end
